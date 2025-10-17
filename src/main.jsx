eb92c94412faa2d0fcd3e0e8fd95a19d3a7b127b
miguelxxx <miguelparreiramedeiros@gmail.com>
2025-10-17 12:05:41 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics/>
+    <SpeedInsights/>
     <App />
   </StrictMode>,
 )
